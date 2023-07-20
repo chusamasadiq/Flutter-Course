d0391f67a11dd7d3a3798b1172edbcac30927107
Usama Sadiq <usamasadiq163@gmail.com>
2023-07-20 18:17:03 +0500

    favourite

--- lib/screen/favourite_screen.dart
+++ lib/screen/favourite_screen.dart
@@ -7,7 +7,6 @@ import '../models/cart.dart';
 import '../provider/cart_provider.dart';
 import '../resources/db_helper.dart';
 import '../utils/utils.dart';
-
 import 'cart_screen.dart';
 
 class FavouriteScreen extends StatefulWidget {
@@ -19,6 +18,34 @@ class FavouriteScreen extends StatefulWidget {
 
 class _FavouriteScreenState extends State<FavouriteScreen> {
   DBHelper? dbHelper = DBHelper();
+  bool isLoading = true;
+  List<DocumentSnapshot> foodList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchFoodData();
+  }
+
+  // Fetch food data using StreamBuilder and pagination
+  void fetchFoodData() async {
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('food')
+          .limit(10) // Adjust the limit based on your pagination needs
+          .get();
+
+      setState(() {
+        foodList = snapshot.docs;
+        isLoading = false;
+      });
+    } catch (error) {
+      Utils.toastMessage('Something went wrong');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,129 +98,125 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
             ),
             const SizedBox(height: 10),
             Expanded(
-              child: FutureBuilder(
-                future: FirebaseFirestore.instance.collection('food').get(),
-                builder: (
-                  BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot,
-                ) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasData) {
-                    final List<DocumentSnapshot> foodList = snapshot.data!.docs;
-                    return GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 10,
-                        mainAxisSpacing: 10,
-                        childAspectRatio: 0.68,
+              child: isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(
+                        color: Color(0xffFF8419),
                       ),
-                      itemCount: foodList.length,
-                      itemBuilder: (BuildContext context, index) {
-                        return InkWell(
-                          borderRadius: BorderRadius.circular(12),
-                          onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => FoodDescription(
-                                foodName: foodList[index]['foodName'],
-                                foodImage: foodList[index]['foodImage'],
-                                foodPrice: foodList[index]['foodPrice'],
-                                deliveryCharges: foodList[index]
-                                    ['deliveryCharges'],
-                                deliveryTime: foodList[index]['deliveryTime'],
-                                foodRatings: foodList[index]['ratings'],
-                                foodDescription: foodList[index]
-                                    ['foodDescription'],
-                              ),
-                            ),
+                    )
+                  : foodList.isEmpty
+                      ? const Center(
+                          child: Text('No data available.'),
+                        )
+                      : GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            childAspectRatio: 0.68,
                           ),
-                          child: Ink(
-                            height: 200,
-                            decoration: BoxDecoration(
-                              color: Colors.grey[100],
+                          itemCount: foodList.length,
+                          itemBuilder: (BuildContext context, index) {
+                            final foodItem = foodList[index];
+                            return InkWell(
                               borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Column(
-                              children: [
-                                ClipRRect(
+                              onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => FoodDescription(
+                                    foodName: foodItem['foodName'],
+                                    foodImage: foodItem['foodImage'],
+                                    foodPrice: foodItem['foodPrice'],
+                                    deliveryCharges:
+                                        foodItem['deliveryCharges'],
+                                    deliveryTime: foodItem['deliveryTime'],
+                                    foodRatings: foodItem['ratings'],
+                                    foodDescription:
+                                        foodItem['foodDescription'],
+                                  ),
+                                ),
+                              ),
+                              child: Ink(
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[100],
                                   borderRadius: BorderRadius.circular(12),
-                                  child: Image.network(
-                                    foodList[index]['foodImage'],
-                                    fit: BoxFit.cover,
-                                    height: 150,
-                                    width: double.infinity,
-                                  ),
                                 ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  foodList[index]['foodName'],
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  'Price: ${foodList[index]['foodPrice'].toString()}/-',
-                                ),
-                                const SizedBox(height: 5),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    dbHelper!
-                                        .insert(
-                                      Cart(
-                                        foodID: foodList[index]['foodID']
-                                            .toString(),
-                                        foodName: foodList[index]['foodName']
-                                            .toString(),
-                                        foodPrice: foodList[index]['foodPrice'],
-                                        quantity: foodList[index]['quantity'],
-                                        foodImage: foodList[index]['foodImage']
-                                            .toString(),
-                                        deliveryCharges: foodList[index]
-                                            ['deliveryCharges'],
-                                        foodTotalPrice: foodList[index]
-                                            ['foodPrice'],
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(12),
+                                      child: Image.network(
+                                        foodList[index]['foodImage'],
+                                        fit: BoxFit.cover,
+                                        height: 150,
+                                        width: double.infinity,
                                       ),
-                                    )
-                                        .then((value) {
-                                      Utils.toastMessage(
-                                          "Product is Added to Cart");
-                                      cart.addTotalPrice(double.parse(
-                                          foodList[index]['foodPrice']
-                                              .toString()));
-                                      cart.addCounter();
-                                    }).catchError((error) {
-                                      if (error is DatabaseException) {
-                                        if (error.toString().contains(
-                                            'UNIQUE constraint failed')) {
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(
+                                      foodItem['foodName'],
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(
+                                      'Price: ${foodItem['foodPrice'].toString()}/-',
+                                    ),
+                                    const SizedBox(height: 5),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        dbHelper!
+                                            .insert(
+                                          Cart(
+                                            foodID:
+                                                foodItem['foodID'].toString(),
+                                            foodName:
+                                                foodItem['foodName'].toString(),
+                                            foodPrice: foodItem['foodPrice'],
+                                            quantity: foodItem['quantity'],
+                                            foodImage: foodItem['foodImage']
+                                                .toString(),
+                                            deliveryCharges:
+                                                foodItem['deliveryCharges'],
+                                            foodTotalPrice:
+                                                foodItem['foodPrice'],
+                                          ),
+                                        )
+                                            .then((value) {
                                           Utils.toastMessage(
-                                              "Product is already in the Cart");
-                                        } else {
-                                          Utils.toastMessage(
-                                              "Error occurred while adding the product");
-                                        }
-                                      } else {
-                                        Utils.toastMessage(
-                                            "Error occurred while adding the product");
-                                      }
-                                    });
-                                  },
-                                  child: const Text('Add to Cart'),
+                                              "Product is Added to Cart");
+                                          cart.addTotalPrice(double.parse(
+                                              foodItem['foodPrice']
+                                                  .toString()));
+                                          cart.addCounter();
+                                        }).catchError((error) {
+                                          if (error is DatabaseException) {
+                                            if (error.toString().contains(
+                                                'UNIQUE constraint failed')) {
+                                              Utils.toastMessage(
+                                                  "Product is already in the Cart");
+                                            } else {
+                                              Utils.toastMessage(
+                                                  "Error occurred while adding the product");
+                                            }
+                                          } else {
+                                            Utils.toastMessage(
+                                                "Error occurred while adding the product");
+                                          }
+                                        });
+                                      },
+                                      child: const Text('Add to Cart'),
+                                    ),
+                                  ],
                                 ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  } else {
-                    return const Center(child: Text('No data available.'));
-                  }
-                },
-              ),
+                              ),
+                            );
+                          },
+                        ),
             ),
           ],
         ),
